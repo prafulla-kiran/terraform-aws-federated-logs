72ee5a7bba11c21df3b4252941c8f1baa526c538
rohit-bandlamudi-nr <rbandlamudi@newrelic.com>
2026-02-16 16:34:13 +0530

    removed unncessary local variables

--- modules/federated_logs_partition/locals.tf
+++ modules/federated_logs_partition/locals.tf
@@ -1,6 +1,4 @@
 locals {
-  s3_bucket_name     = "nr-fed-logs"
-  glue_catalog_db_name = "nr_fed_logs_iceberg_db"
   default_partition_name = "default"
   iceberg_table_name_prefix = "nr-fed-logs"
 
