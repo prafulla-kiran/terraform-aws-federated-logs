7c454237f455d9646ce09d0b4060bc00a02f2b89
Aashirwad Jain <aashirwad2001jain@gmail.com>
2026-05-25 14:35:36 +0530

    fix(federated-logs-role): update the reader role to trust NRGlobalIAMRole (#33)

--- modules/federated_logs_role/locals.tf
+++ modules/federated_logs_role/locals.tf
@@ -1,10 +1,17 @@
 locals {
   setup_naming_prefix = "newrelic-fed-logs-${var.setup_name}"
 
-  //TODO: Need to finalise on the account id we wish to move forward with
-  nr_source_account = "864899866645" # New Relic AWS account ID for cross-account access
+  # Logging Federated
+  nr_source_account = "531948421264"
+
+  # WARNING [DO NOT CHANGE]: Cross-repo contract with the NR hub. NRGlobalIAMRole's
+  # inline policy only allows sts:AssumeRole on role ARNs matching
+  # `newrelic-fed-logs-*-nr-query`. Editing this suffix will break cross-account
+  # assumption at runtime.
+  nr_reader_role_suffix = "nr-query"
 
   nr_graphql_endpoint = var.newrelic_region == "EU" ? "https://api.eu.newrelic.com/graphql" : (
     var.newrelic_region == "STAGING" ? "https://staging-api.newrelic.com/graphql" : "https://api.newrelic.com/graphql"
   )
+
 }
